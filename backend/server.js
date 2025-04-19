8a56703906590dc3be30f7eb6e5f299c252963b5
AparmitSrivastava <aparmitsrivastava1@gmail.com>
2025-04-19 16:16:37 +0530

    added fixes

--- backend/server.js
+++ backend/server.js
@@ -1,55 +1,123 @@
+// const express = require('express')
+// const { MongoClient } = require('mongodb');
+// const dotenv = require('dotenv')
+// const bodyparser = require('body-parser')
+// const cors = require('cors')
+// // or as an es module:
+// // import { MongoClient } from 'mongodb'
+
+// dotenv.config();
+
+// // Connection URL
+// // const url = 'mongodb://localhost:27017'; //mongo compass
+// const url = process.env.MONGODB_URI; //mongo atlas
+// const client = new MongoClient(url);
+
+// // Database Name
+// const dbName = 'SafePass';
+// const app = express()
+// // const port = 3000
+// const port = process.env.PORT || 3000
+
+
+// app.use(bodyparser.json())
+// app.use(cors())
+
+// client.connect();
+
+// // getting all the passwords
+// app.get('/', async (req, res) => {
+//     const db = client.db(dbName);
+//     const collection = db.collection('passwords')
+//     const findResult = await collection.find({}).toArray();
+//     res.json(findResult)
+// })
+
+// // saving all the passwords
+// app.post('/', async (req, res) => {
+//     const password = req.body;
+//     const db = client.db(dbName);
+//     const collection = db.collection('passwords')
+//     const findResult = await collection.insertOne(password);
+//     res.send({success : true , result : findResult})
+// })
+
+// // dlete the pass
+// app.delete('/', async (req, res) => {
+//     const password = req.body;
+//     const db = client.db(dbName);
+//     const collection = db.collection('passwords')
+//     const findResult = await collection.deleteOne(password);
+//     res.send({success : true , result : findResult})
+// })
+
+// app.listen(port, () => {
+//     console.log(`Example app listening on port http://localhost:${port}`)
+// }) 
+
+
+
+
+
+
 const express = require('express')
 const { MongoClient } = require('mongodb');
 const dotenv = require('dotenv')
 const bodyparser = require('body-parser')
-// or as an es module:
-// import { MongoClient } from 'mongodb'
+const cors = require('cors')
 
 dotenv.config();
 
 // Connection URL
-// const url = 'mongodb://localhost:27017'; //mongo compass
-const url = process.env.MONGODB_URI; //mongo atlas
+const url = process.env.MONGODB_URI; // Mongo Atlas URL
 const client = new MongoClient(url);
 
 // Database Name
 const dbName = 'SafePass';
-const app = express()
-// const port = 3000
-const port = process.env.PORT || 3000
-const cors = require('cors')
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(bodyparser.json())
-app.use(cors())
+// CORS setup - restrict to your frontend URL
+const allowedOrigins = ['https://password-manager-mongo.vercel.app'];  // Your Vercel frontend URL
+const corsOptions = {
+  origin: allowedOrigins,
+  methods: ['GET', 'POST', 'DELETE'],
+  allowedHeaders: ['Content-Type'],
+};
 
+app.use(bodyparser.json());
+app.use(cors(corsOptions));  // Apply CORS with the restricted origins
+
+// Connect to MongoDB
 client.connect();
 
-// getting all the passwords
+// GET all passwords
 app.get('/', async (req, res) => {
-    const db = client.db(dbName);
-    const collection = db.collection('passwords')
-    const findResult = await collection.find({}).toArray();
-    res.json(findResult)
-})
+  const db = client.db(dbName);
+  const collection = db.collection('passwords');
+  const findResult = await collection.find({}).toArray();
+  res.json(findResult);
+});
 
-// saving all the passwords
+// POST a new password
 app.post('/', async (req, res) => {
-    const password = req.body;
-    const db = client.db(dbName);
-    const collection = db.collection('passwords')
-    const findResult = await collection.insertOne(password);
-    res.send({success : true , result : findResult})
-})
+  const password = req.body;
+  const db = client.db(dbName);
+  const collection = db.collection('passwords');
+  const findResult = await collection.insertOne(password);
+  res.send({ success: true, result: findResult });
+});
 
-// dlete the pass
+// DELETE a password
 app.delete('/', async (req, res) => {
-    const password = req.body;
-    const db = client.db(dbName);
-    const collection = db.collection('passwords')
-    const findResult = await collection.deleteOne(password);
-    res.send({success : true , result : findResult})
-})
+  const password = req.body;
+  const db = client.db(dbName);
+  const collection = db.collection('passwords');
+  const findResult = await collection.deleteOne(password);
+  res.send({ success: true, result: findResult });
+});
 
+// Start the server
 app.listen(port, () => {
-    console.log(`Example app listening on port http://localhost:${port}`)
-}) 
+  console.log(`Server is running at http://localhost:${port}`);
+});
